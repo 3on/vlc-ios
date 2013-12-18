f036ae192742badfd0afb4c3f6ff7ececc26eb39
Jean-Romain Prévost <jr@ailleurs.me>
2013-12-17 21:15:46 -0800

    Remove default components, add navbar component with sytling

---
+++ Resources/web/ui/navbar.reel/navbar.js
@@ -0,0 +1,33 @@
+/**
+ * @module ui/navbar.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Navbar
+ * @extends Component
+ */
+exports.Navbar = Component.specialize(/** @lends Navbar# */ {
+    title: {
+        value: ""
+    },
+
+    constructor: {
+        value: function Navbar() {
+            this.super();
+        }
+    },
+
+    handleAddButtonAction: {
+        value: function (evt) {
+            this.dispatchEventNamed("addFile", true, true);
+        }
+    },
+
+    handlehomeButtonAction: {
+        value: function (evt) {
+            this.dispatchEventNamed("addFile", true, true);
+        }
+    }
+});
